b2334f4a78bac694f5fb82395c9ee2a6d6ccf462
wangll <981609608@qq.com>
2016-03-08 18:23:51 +0800

    second

--- src/test/A.java
+++ src/test/A.java
@@ -3,5 +3,10 @@ package test;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("jhfdhfj");
+		System.out.println("jhfdhfj");
+		System.out.println("jhfdhfj");
+		System.out.println("jhfdhfj");
+		System.out.println("jhfdhfj");
+
 	}
 }
